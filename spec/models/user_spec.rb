6bbce93ccc94978ee1c9c79d1ad8c6c16730b3f6
oloomoses <olooine@gmail.com>
2021-11-12 20:11:02 +0300

    Add tests for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create_user }
+
+  it 'is valid with a username, an email and a password & confirmation' do
+    expect(user).to be_valid
+  end
+
+  it 'is authenticable with valid password confirmation' do
+    expect(user.valid_password?('123456789')).to be_truthy
+  end
+
+  describe 'Username validations' do
+    it 'validates username' do
+      expect(user.username).to be_present
+    end
+  end
 end
